9bd939b4c738caff4992bf932ca75c6024ba9e4d
aprospero <apro@posteo.de>
2023-06-13 21:39:11 +0200

    addendum to 5576f4bf - Output Version string on startup
    previous version triggered build on every occasion even when there hasn't been any change at all.

--- ems_serio/project/get_git_desc.sh
+++ ems_serio/project/get_git_desc.sh
@@ -2,19 +2,28 @@
 
 cd "${0%/*}"
 
+fname="../version.h"
+
 git_desc=$(git describe --abbrev=8 --dirty --always --tags)
 
-if [ "X$1" = "X" ] ; then
-  suffix=""
-else
-  suffix="-$1"
+if [ "X$1" != "X" ] ; then
+  git_desc="${git_desc}-$1"
 fi
 
-cat > ../version.h << EOL
+if [ -f $fname ] ; then
+  fwords=($(sed '4q;d' ${fname}))  # array of words in line 4 of file 'fname'
+  fdesc=${fwords[2]}               # 3rd word of 4th line
+  if [ "$fdesc" = "\"$git_desc\"" ] ; then
+    echo "git description already up to date."
+    exit 0
+  fi
+fi
+
+cat > ${fname} << EOL
 #ifndef _VERSION_H
 #define _VERSION_H
 
-#define APP_VERSION "${git_desc}${suffix}"
+#define APP_VERSION "${git_desc}"
 
 #endif  // _VERSION_H
 EOL
