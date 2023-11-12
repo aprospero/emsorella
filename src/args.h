465487e91a66071a3b8720bda0cec1da5d9bd8a9
aprospero <apro@posteo.de>
2023-11-12 13:38:43 +0100

    make mqtt connection configurable.

---
+++ src/args.h
@@ -0,0 +1,32 @@
+#ifndef _H_ARGS
+#define _H_ARGS
+
+#include "ctrl/com/mqtt.h"
+#include "ctrl/logger.h"
+
+#define DEFAULT_LOG_FACILITY LF_LOCAL1
+#define DEFAULT_LOG_LEVEL    LL_ERROR
+
+#define DEFAULT_SERIAL_DEVICE "/dev/ttymxc0"
+
+#define DEFAULT_MQTT_REMOTE "localhost"
+#define DEFAULT_MQTT_PORT   1883
+
+#define DEFAULT_MQTT_CLIENT_ID "ems"
+#define DEFAULT_MQTT_TOPIC     "MTDC"
+#define DEFAULT_MQTT_QOS       2
+
+
+struct emsorella_config
+{
+    const char *       prg_name;
+    enum log_facility  log_facility;
+    enum log_level     log_level;
+    char *             serial_device;
+    struct mqtt_config mqtt;
+};
+
+int parseArgs(int argc, char * argv[], struct emsorella_config * config);
+
+
+#endif
